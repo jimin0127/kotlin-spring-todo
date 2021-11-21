3e1dc9a6f50574c7f267e3d2c242fce99407c16b
jimin0127 <ckdgh4662@naver.com>
2021-11-22 00:17:34 +0900

    feat: DONE 상태로 변경하는 기능 추가

--- src/main/kotlin/com/example/todo/Service.kt
+++ src/main/kotlin/com/example/todo/Service.kt
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.util.Optional
 import javax.transaction.Transactional
 
 interface TodoService {
