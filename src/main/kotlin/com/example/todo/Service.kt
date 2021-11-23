70e9b8e807813162cf90707b63ee1d8de67935ee
jimin0127 <ckdgh4662@naver.com>
2021-11-23 18:49:38 +0900

    review: 반환 타입 변경

--- src/main/kotlin/com/example/todo/Service.kt
+++ src/main/kotlin/com/example/todo/Service.kt
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import java.util.Optional
 import javax.transaction.Transactional
 
 interface TodoService {
